fc3a3e7aa5dc3910fdce1b4f9da43e1a9c9c51b5
SadmanTariq <sadmantariq@gmail.com>
2020-05-29 16:36:15 +0600

    Sprite rotates on jump and dive

--- Endless Runner/Duck/States/Move.gd
+++ Endless Runner/Duck/States/Move.gd
@@ -5,6 +5,8 @@ export(float) var jump_speed
 export(String) var action
 export(bool) var flip_v
 export(NodePath) var sprite
+export(float) var rotation_speed_degrees
+export(float) var rotation_degrees
 var floor_height: int
 var y_vel: float
 
@@ -18,7 +20,9 @@ func enter():
 func physics_process(delta):
 	y_vel += gravity
 	owner.global_position.y += y_vel * delta
+	get_node(sprite).rotation_degrees += rotation_speed_degrees * delta
 	if _grounded():
+		get_node(sprite).rotation_degrees = 0
 		if flip_v:
 			get_node(sprite).flip_v = false
 		fsm.change_to("Walk")
@@ -30,6 +34,7 @@ func input(event: InputEvent):
 
 
 func _jump():
+	get_node(sprite).rotation_degrees = rotation_degrees
 	y_vel = jump_speed
 
 
