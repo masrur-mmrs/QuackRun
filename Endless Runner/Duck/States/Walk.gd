9137d2b7d1c9d601dcd2c67e883b8d2f00e85d5a
masrur-mmrs <42804706+masrur-mmrs@users.noreply.github.com>
2020-06-01 00:48:10 +0600

    Update Walk.gd

--- Endless Runner/Duck/States/Walk.gd
+++ Endless Runner/Duck/States/Walk.gd
@@ -4,14 +4,18 @@ export(NodePath) var sprite
 var floor_height: int
 
 
+
 func enter():
 	get_node(sprite).playing = true
+	get_node(sprite).animation = "walk"
 	owner.global_position.y = floor_height
+
 
 
 func input(event: InputEvent):
 	if event.is_action_pressed("jump"):
 		get_node(sprite).playing = false
+		get_node(sprite).animation = "fly"
 		fsm.change_to("Fly")
 	elif event.is_action_pressed("dive"):
 		get_node(sprite).playing = false
