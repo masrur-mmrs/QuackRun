6753eb8bc26b975a9f7f0765651fdd99867d0a64
masrur-mmrs <42804706+masrur-mmrs@users.noreply.github.com>
2020-06-08 04:07:22 +0600

    Add files via upload

--- Endless Runner/MainScenes/MainMenu.gd
+++ Endless Runner/MainScenes/MainMenu.gd
@@ -2,7 +2,11 @@ extends Control
 
 export(String) var main_scene_path
 
+func _ready():
+	print("Be in MainMenu")
+	pass
 
-func input(event: InputEvent):
-	if event.is_action_pressed("play"):
-		get_tree().change_scene(main_scene_path)
+func _process(delta):
+	if Input.is_action_just_pressed("Start"):
+		print("load World")
+		get_tree().change_scene("res://MainScenes//World.tscn")
