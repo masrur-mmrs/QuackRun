60026ca27732e34a0072f171d85ad09f9ca90e4b
SadmanTariq <sadmantariq@gmail.com>
2020-06-08 12:34:12 +0600

    Better main menu

--- Endless Runner/MainScenes/MainMenu.gd
+++ Endless Runner/MainScenes/MainMenu.gd
@@ -2,11 +2,7 @@ extends Control
 
 export(String) var main_scene_path
 
-func _ready():
-	print("Be in MainMenu")
-	pass
 
-func _process(delta):
-	if Input.is_action_just_pressed("Start"):
-		print("load World")
+func _input(event):
+	if !(event is InputEventMouseMotion):
 		get_tree().change_scene("res://MainScenes//World.tscn")
