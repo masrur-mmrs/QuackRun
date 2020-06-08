25b44660ea27977b52d1b960701cf3b69365564f
SadmanTariq <sadmantariq@gmail.com>
2020-06-08 10:46:12 +0600

    Add biases to obstacle spawner

--- Endless Runner/environment/Obstacles/ObstacleSpawner.gd
+++ Endless Runner/environment/Obstacles/ObstacleSpawner.gd
@@ -1,19 +1,34 @@
 extends Node2D
 
 export(Array, PackedScene) var obstacles
+
+export var biased = false
+# A number for each obstacle. Higher means more likely, 0 means spawning disabled
+export(Array, int) var biases
+
 export var interval_seconds = 3
 export var interval_randomness = 0.5
 
+var biased_obstacle_list = []
+
+
 func _enter_tree():
+	if !biased:
+		biased_obstacle_list = obstacles
+	else:
+		for i in range(len(obstacles)):
+			for _j in range(biases[i]):
+				biased_obstacle_list.append(obstacles[i])
+	
 	_spawn_random_obstacle()
 
 
 func _spawn_random_obstacle():
 	var obstacle_instance
-	if len(obstacles) == 1:
-		obstacle_instance = obstacles[0].instance()
+	if len(biased_obstacle_list) == 1:
+		obstacle_instance = biased_obstacle_list[0].instance()
 	else:
-		obstacle_instance = obstacles[randi() % len(obstacles)].instance()
+		obstacle_instance = biased_obstacle_list[randi() % len(biased_obstacle_list)].instance()
 	
 	add_child(obstacle_instance)
 
