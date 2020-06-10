9a2f790b8fd9d02eeb2c201e23744ac99663e29f
SadmanTariq <sadmantariq@gmail.com>
2020-06-10 12:42:38 +0600

    Random start height for powerups

--- Endless Runner/environment/PowerUps/Bread/BonusScore.gd
+++ Endless Runner/environment/PowerUps/Bread/BonusScore.gd
@@ -2,6 +2,12 @@ extends "res://Environment/Obstacles/StraightLine.gd"
 
 export var score = 15
 export var remove_on_use = true
+export var min_y = 5.0
+export var max_y = 550.0
+
+func _enter_tree():
+	speed = start_speed
+	position.y = rand_range(min_y, max_y)
 
 
 func _on_area_entered(area):
--- Endless Runner/environment/PowerUps/InvulnPowerup/InvulnPowerup.gd
+++ Endless Runner/environment/PowerUps/InvulnPowerup/InvulnPowerup.gd
@@ -1,5 +1,11 @@
 extends "res://Environment/Obstacles/StraightLine.gd"
 
+export var min_y = 5.0
+export var max_y = 550.0
+
+func _enter_tree():
+	speed = start_speed
+	position.y = rand_range(min_y, max_y)
 
 func _on_InvulnPowerup_area_entered(area):
 	if area.is_in_group("duck"):
